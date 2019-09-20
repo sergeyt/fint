0048cf1e1c08ccbbbb94a2aae16554e183fb5fa9
Sergey Todyshev <stodyshev@gmail.com>
2019-09-20 23:07:10 +0700

    hello program

---
+++ test/hello/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace hello
+{
+    class Program
+    {
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        static extern void Print(string s);
+
+        static void Main(string[] args)
+        {
+            Print("Hello World!");
+        }
+    }
+}
