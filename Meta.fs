779e9bfeb1613b00b1e42b3b2f7822ecfea3958f
Sergey Todyshev <stodyshev@gmail.com>
2019-09-22 09:03:30 +0700

    wip meta, decode coded index

--- Meta.fs
+++ Meta.fs
@@ -19,19 +19,19 @@ type CodedIndexId =
 
 type CodedIndex =
     { bits : int
-      tables : TableId list }
+      tables : TableId array }
 
 let customAttributeType : CodedIndex =
     { bits = 3
       tables =
-          [ TableId.TypeRef; TableId.TypeRef; TableId.MethodDef;
-            TableId.MemberRef; TableId.TypeDef ] }
+          [| TableId.TypeRef; TableId.TypeRef; TableId.MethodDef;
+             TableId.MemberRef; TableId.TypeDef |] }
 
 let codedIndexMap =
     dict [ (CustomAttributeType, customAttributeType)
            (HasConstant,
             { bits = 2
-              tables = [ TableId.Field; TableId.Param; TableId.Property ] })
+              tables = [| TableId.Field; TableId.Param; TableId.Property |] })
 
            (//NOTE FROM SPEC:
             //[Note: HasCustomAttributes only has values for tables that are �externally visible�; that is, that correspond to items
@@ -40,49 +40,86 @@ let codedIndexMap =
             HasCustomAttribute,
             { bits = 5
               tables =
-                  [ TableId.MethodDef; TableId.Field; TableId.TypeRef;
-                    TableId.TypeDef; TableId.Param; TableId.InterfaceImpl;
-                    TableId.MemberRef; TableId.Module; TableId.DeclSecurity;
-                    TableId.Property; TableId.Event; TableId.StandAloneSig;
-                    TableId.ModuleRef; TableId.TypeSpec; TableId.Assembly;
-                    TableId.AssemblyRef; TableId.File; TableId.ExportedType;
-                    TableId.ManifestResource; TableId.GenericParam ] })
+                  [| TableId.MethodDef; TableId.Field; TableId.TypeRef;
+                     TableId.TypeDef; TableId.Param; TableId.InterfaceImpl;
+                     TableId.MemberRef; TableId.Module; TableId.DeclSecurity;
+                     TableId.Property; TableId.Event; TableId.StandAloneSig;
+                     TableId.ModuleRef; TableId.TypeSpec; TableId.Assembly;
+                     TableId.AssemblyRef; TableId.File; TableId.ExportedType;
+                     TableId.ManifestResource; TableId.GenericParam |] })
 
            (HasDeclSecurity,
             { bits = 2
-              tables = [ TableId.TypeDef; TableId.MethodDef; TableId.Assembly ] })
+              tables =
+                  [| TableId.TypeDef; TableId.MethodDef; TableId.Assembly |] })
            (HasFieldMarshal,
             { bits = 1
-              tables = [ TableId.Field; TableId.Param ] })
+              tables = [| TableId.Field; TableId.Param |] })
            (HasSemantics,
             { bits = 1
-              tables = [ TableId.Event; TableId.Property ] })
+              tables = [| TableId.Event; TableId.Property |] })
 
            (Implementation,
             { bits = 2
               tables =
-                  [ TableId.File; TableId.AssemblyRef; TableId.ExportedType ] })
+                  [| TableId.File; TableId.AssemblyRef; TableId.ExportedType |] })
            (MemberForwarded,
             { bits = 1
-              tables = [ TableId.Field; TableId.MethodDef ] })
+              tables = [| TableId.Field; TableId.MethodDef |] })
 
            (MemberRefParent,
             { bits = 3
               tables =
-                  [ TableId.TypeDef; TableId.TypeRef; TableId.ModuleRef;
-                    TableId.MethodDef; TableId.TypeSpec ] })
+                  [| TableId.TypeDef; TableId.TypeRef; TableId.ModuleRef;
+                     TableId.MethodDef; TableId.TypeSpec |] })
            (MethodDefOrRef,
             { bits = 1
-              tables = [ TableId.MethodDef; TableId.MemberRef ] })
+              tables = [| TableId.MethodDef; TableId.MemberRef |] })
 
            (ResolutionScope,
             { bits = 2
               tables =
-                  [ TableId.Module; TableId.ModuleRef; TableId.AssemblyRef;
-                    TableId.TypeRef ] })
+                  [| TableId.Module; TableId.ModuleRef; TableId.AssemblyRef;
+                     TableId.TypeRef |] })
+
            (TypeDefOrRef,
             { bits = 2
-              tables = [ TableId.TypeDef; TableId.TypeRef; TableId.TypeSpec ] })
+              tables = [| TableId.TypeDef; TableId.TypeRef; TableId.TypeSpec |] })
            (TypeOrMethodDef,
             { bits = 1
-              tables = [ TableId.TypeDef; TableId.MethodDef ] }) ]
+              tables = [| TableId.TypeDef; TableId.MethodDef |] }) ]
+
+type TableIndex =
+    { table : TableId
+      index : int }
+
+let decodeCodedIndex (meta : CodedIndex, value : uint32) =
+    let mask = uint32 (0xff >>> (8 - meta.bits))
+    let tag = int (value &&& uint32 mask)
+    if (tag < 0 || tag >= meta.tables.Length) then
+        invalidArg "value" "bad coded index"
+    let index = int (value >>> meta.bits)
+
+    let result : TableIndex =
+        { table = meta.tables.[tag]
+          index = index }
+    result
+
+let decodeTableIndex (token : uint32) =
+    let id = int ((token >>> 24) &&& uint32 0xff)
+    let index = int (token &&& uint32 0xffffff)
+
+    let result : TableIndex =
+        { table = enum id
+          index = index }
+    result
+
+// data types of metadata column
+type ColumnType =
+  | Int32
+  | Int16
+  | StringIdx
+  | BlobIdx
+  | TableIdx
+  | CodedIndex
+
