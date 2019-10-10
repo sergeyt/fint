f3533f7c43242e7ea0df7bcf19554c7560344e55
Sergey Todyshev <stodyshev@gmail.com>
2019-10-10 16:51:45 +0700

    compile samples using csc

---
+++ csc.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+LIB=/usr/local/share/dotnet/shared/Microsoft.NETCore.App/3.0.0
+CSC=/usr/local/share/dotnet/sdk/3.0.100/Roslyn/bincore/csc.dll
+SAMPLE=test/$1.cs
+OUT=test/$1.dll
+
+dotnet ${CSC} -r:${LIB}/System.Private.CoreLib.dll -r:${LIB}/System.Console.dll -r:${LIB}/System.Runtime.dll -out:${OUT} ${SAMPLE}
+cp -f test/runtimeconfig.json test/$1.runtimeconfig.json
